26ead7daba119ada153172e11fb9bbde3b8195e7
Paris Cornett <pariscornett@Pariss-MacBook-Air.local>
2019-05-09 22:09:55 -0500

    attempting to replace letters with underscores in alert

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,10 +1,56 @@
-//get set up
+//creates word bank and randomizes array
 var wordBank = ["fern", "rose", "lavender"];
 
+var word = (Math.floor(Math.random() * wordBank.lengths));
 
-//PART ONE-----------------------------------------------------------------------
-//code to randomly select a word from wordList
-var wordBank = (Math.floor(Math.random() * wordBank.lengths));
+//make randomized word appear as underscores in the DOM
+var userDisplay = [];
+for (var i = 0; i < word.length; i++) {
+    userDisplay[i] = "_";
+}
+
+//variable to hold number of remaining letters (not sure I need this)
+var remainingLetters = word.length;
+
+//---------------------main game loop----------------------//
+
+//while there are letters left to be guessed
+while(remainingLetters > 0) {
+    //show the player their progress
+    alert(userDisplay.join(" "));
+    //get a guess from the player
+    var guess = prompt("guess a letter");
+    //if the guess is blank
+    if (guess == null) {
+        //exit the game loop
+        break;
+    //if the guess is more than one letter or no letters
+    }else if (guess.length !== 1){
+        //alert them to guess a single letter only
+        alert("Only guess one letter");
+    //valid guess
+
+    }else{
+        //update the game state with the guess
+        for (var j = 0; j < word.length; j++){
+            //if the letter they guessed is in the word
+            //at that point or index
+            if (word[j] === guess){
+                //update the answer array with the letter they guessed at that point or index
+                userDisplay[j] = guess;
+                //subtract one from remaining letters
+                remainingLetters--;
+            }
+        }
+    }
+    //----------------END OF GAME LOOP--------------------
+
+    }
+    //let player know the word 
+    alert(userDisplay.join (" "));
+    //congratulate the player
+    alert("Good job! The answer was " + word);
+
 
 
 //this is randomly guessing the index 
